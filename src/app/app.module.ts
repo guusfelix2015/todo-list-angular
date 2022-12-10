4d2f02a0724659bbe555bbe492eacde3fe0cc946
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-10 10:11:17 -0300

    criando componente todo e create-todo e serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CreateTodoComponent
   ],
   imports: [
     BrowserModule
