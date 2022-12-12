2c92517af0df7d6560f5a54ea49ae32a84aeb59f
guusfelix2015 <gusfelix2015@gmail.com>
2022-12-11 23:09:20 -0300

    Revomendo todo

--- src/app/components/create-todo/create-todo.component.ts
+++ src/app/components/create-todo/create-todo.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+interface TypesTodo {
+  text: string;
+  check: boolean;
+}
 
 @Component({
   selector: 'app-create-todo',
@@ -10,7 +15,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 })
 export class CreateTodoComponent {
   faTrash = faTrash;
-  todos: string[] = [];
+  todos: TypesTodo[] = [];
 
   constructor(private todoService: TodoService) {}
 
@@ -20,5 +25,10 @@ export class CreateTodoComponent {
 
   onAddTodo(todo: string) {
     this.todoService.addTodo(todo);
+    console.log(this.todos);
+  }
+
+  removeTodo(todo: string) {
+    this.todos = this.todos.filter((t) => t.text !== todo);
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 
+interface TypesTodo {
+  text: string;
+  check: boolean;
+}
+
 @Injectable()
 export class TodoService {
-  private todos: string[] = ['Teste', 'hello'];
+  private todos: TypesTodo[] = [];
 
   getTodos() {
     return this.todos;
   }
 
   addTodo(todo: string) {
-    this.todos.push(todo);
+    this.todos.push({ text: todo, check: false });
   }
 }
